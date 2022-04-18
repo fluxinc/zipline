62f2976cfa007d1bffe6294fb6802f7ef8af8665
Jack Lucas <jack@occiro.com>
2022-04-18 11:27:05 -0400

    Initial Windows Service Template

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.ServiceProcess;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Mono.Options;
+
+namespace DICOMCapacitorWarden
+{
+  internal static class Program
+  {
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    static void Main()
+    {
+      var optionsSet = new OptionSet
+      {
+
+      };
+
+    }
+  }
+}
